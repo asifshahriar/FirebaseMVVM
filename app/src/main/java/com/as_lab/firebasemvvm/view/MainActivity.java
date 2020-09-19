a5c3e87f7b1746d240dc7a355115086274f22e3d
asif <asifs.cse@gmail.com>
2020-09-19 22:36:14 +0600

    first

---
+++ app/src/main/java/com/as_lab/firebasemvvm/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.as_lab.firebasemvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.as_lab.firebasemvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
